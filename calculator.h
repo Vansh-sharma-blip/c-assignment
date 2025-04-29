07477aba917ce5e6edfc7781036e7e6d40cfd4a7
Vansh-sharma-blip <vansh.sharma_cs24@gla.ac.in>
2025-04-29 17:56:01 +0530

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -1,9 +1,16 @@
-int sum(int x,int y){
-	return x+y;
+typedef int (*operation_func)(int, int);
+
+int sum(int x, int y) {
+    return x + y;
 }
-int sub(int x, int y){
-	return x-y;
+
+int sub(int x, int y) {
+    return x - y;
 }
-int mul(int x, int y){
-	return x*y;
+
+int mul(int x, int y) {
+    return x * y;
 }
+operation_func op_sum = sum;
+operation_func op_sub = sub;
+operation_func op_mul = mul;
